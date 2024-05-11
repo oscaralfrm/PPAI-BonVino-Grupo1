d51f0e27b4a7c79602ef22efacdf3a64cb309086
Oscar Romero <oscaralfrm1996@gmail.com>
2024-05-10 22:23:02 -0300

    Añadiendo la Interfaz Grafica

---
+++ InterfazGrafica/PPAI-BonVino-Grupo1/src/main/java/Logica/Entidades/RegionVitivinicola.java
@@ -0,0 +1,53 @@
+package Entidades;
+
+public class RegionVitivinicola {
+
+    //Atributos
+
+    private String descipcion;
+
+    private String nombre;
+
+    //Agrego atributo provincia por la relacion para la busqueda de datos
+    private Provincia provincia;
+
+    //Constructores
+
+    public RegionVitivinicola() {
+    }
+
+    public RegionVitivinicola(Provincia provincia, String nombre, String descipcion) {
+        this.provincia = provincia;
+        this.nombre = nombre;
+        this.descipcion = descipcion;
+    }
+
+    //getters and setters
+    public String getDescipcion() {
+        return descipcion;
+    }
+
+    public void setDescipcion(String descipcion) {
+        this.descipcion = descipcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Provincia getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(Provincia provincia) {
+        this.provincia = provincia;
+    }
+
+    public String buscarNombrePais() {
+        return this.provincia.buscarNombrePais();
+    }
+}
---
+++ InterfazGrafica/PPAI-BonVino-Grupo1/src/main/java/Logica/Principal.java
@@ -0,0 +1,62 @@
+package Logica;
+
+import Boundaries.PantallaPrincipal;
+import Boundaries.PantallaRankingVinos;
+import javax.swing.ImageIcon;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+
+        /*
+        
+         // Instancia de Prueba de la Pantalla de Ranking Vinos...
+        PantallaRankingVinos pantallaRankingVinos = new PantallaRankingVinos();
+        
+        
+        // Título de la Ventana...
+        
+        pantallaRankingVinos.setTitle("Generar Ranking de Vinos");
+        
+        // Permite que la ventana sea visible...
+        
+        pantallaRankingVinos.setVisible(true);
+        
+        // Permite que la ventana se quede fija en el centro...
+        
+        pantallaRankingVinos.setLocationRelativeTo(null);
+        
+        // Evita que se pueda maximizar la ventana, ésto lo podemos tratar después, para que no se rompa el código
+        
+        pantallaRankingVinos.setResizable(false);
+        
+        */
+        
+        /*
+        
+        Pantalla Principal
+        
+        
+        */
+        
+        PantallaPrincipal pantallaPrincipal = new PantallaPrincipal();
+        
+        pantallaPrincipal.setTitle("BonVino");
+        
+        // Permite que la ventana sea visible...
+        
+        pantallaPrincipal.setVisible(true);
+        
+        // Permite que la ventana se quede fija en el centro...
+        
+        pantallaPrincipal.setLocationRelativeTo(null);
+        
+        // Evita que se pueda maximizar la ventana, ésto lo podemos tratar después, para que no se rompa el código
+        
+        pantallaPrincipal.setResizable(false);
+        
+        
+        
+    }
+}
